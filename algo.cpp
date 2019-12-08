4e8d493f6244823bde9e66d18547a08e7fa4dadb
Clement Lee <clement.lee@newcastle.ac.uk>
2019-12-08 18:33:23 +0000

    edit R, algo.cpp & data.csv with 1st good results

--- algo.cpp
+++ algo.cpp
@@ -20,7 +20,7 @@ const NumericVector tv(const double x) {
 
 // 01) model
 // [[Rcpp::export]]
-const double llik_model(const NumericVector y1, const NumericVector y2, const IntegerVector x1, const IntegerVector x2, const NumericVector gamma, const double beta) {
+const double llik_model(const NumericVector y1, const NumericVector y2, const IntegerVector x1, const IntegerVector x2, const NumericVector gamma, const double eta) {
   // log-likelihood of model
   // y1 & y2 are number of games won by players 1 & 2, respectively
   // x1 & x2 are the indices of players 1 & 2 in gamma
@@ -29,15 +29,33 @@ const double llik_model(const NumericVector y1, const NumericVector y2, const In
   if (y2.size() != n || x1.size() != n || x2.size() != n) {
     stop("llik_model: y1, y2, x1 and x2 have to be of the same length.");
   }
+  double llik;
   const NumericVector
     gamma1 = gamma[x1],
     gamma2 = gamma[x2],
     r1 = 1.0 / (1.0 + exp(gamma2 - gamma1)),
-    r2 = 1.0 / (1.0 + exp(gamma1 - gamma2)),
-    p1 = pbeta(r1, beta, beta, true, true),
-    p2 = pbeta(r2, beta, beta, true, true),
-    l = y1 * p1 + y2 * p2;
-  double llik = sum(l);
+    r2 = 1.0 / (1.0 + exp(gamma1 - gamma2));
+  NumericVector p1(n, 0.0), p2(n, 0.0);
+  if (eta > 1.0 || eta < 0.0) {
+    llik = -INFINITY;
+  }
+  else {
+    if (eta == 1.0) {
+      p1 = log(r1);
+      p2 = log(r2);
+    }
+    else if (eta == 0.0) {
+      p1 = pnorm(gamma1 - gamma2, 0.0, 1.0, true, true);
+      p2 = pnorm(gamma2 - gamma1, 0.0, 1.0, true, true);
+    }
+    else {
+      const double beta = 1.0 / eta;
+      p1 = pbeta(r1, beta, beta, true, true);
+      p2 = pbeta(r2, beta, beta, true, true);
+    }
+    const NumericVector l = y1 * p1 + y2 * p2;
+    llik = sum(l);
+  }
   if (llik != llik) {
     llik = -INFINITY;
   }
@@ -45,10 +63,10 @@ const double llik_model(const NumericVector y1, const NumericVector y2, const In
 }
 
 // [[Rcpp::export]]
-const double lprior_model(const NumericVector gamma, const double beta, const double mu, const double sigma, const double a_beta = 1.0, const double b_beta = 0.001, double m_mu = 0.0, const double s_mu = 100.0, const double a_sigma = 1.0, const double b_sigma = 0.001) {
+const double lprior_model(const NumericVector gamma, const double eta, const double mu, const double sigma, const double a_eta = 1.0, const double b_eta = 1.0, const double m_mu = 0.0, const double s_mu = 100.0, const double a_sigma = 1.0, const double b_sigma = 0.001) {
   double lprior =
     sum(dnorm(gamma, mu, sigma, true)) +
-    dgamma(tv(beta), a_beta, 1.0 / b_beta, true)[0] +
+    dbeta(tv(eta), a_eta, b_eta, true)[0] +
     dnorm(tv(mu), m_mu, s_mu, true)[0] +
     dgamma(tv(sigma), a_sigma, 1.0 / b_sigma, true)[0];
   if (lprior != lprior) {
@@ -63,95 +81,107 @@ List mh_model(const NumericVector y1,
               const IntegerVector x1,
               const IntegerVector x2,
               const int m,
-              const double beta,
-              const double mu,
-              const double sigma,
-              const double a_beta = 1.0,
-              const double b_beta = 0.001,
+              const double eta = 0.5,
+              const double mu = 0.0,
+              const double sigma = 1.0,
+              const double a_eta = 1.0,
+              const double b_eta = 1.0,
               const double m_mu = 0.0,
               const double s_mu = 100.0,
               const double a_sigma = 1.0,
               const double b_sigma = 0.001,
               double sd_gamma = 0.001,
-              double sd_beta = 0.001,
+              double sd_eta = 0.001,
               double sd_mu = 0.001,
               double sd_sigma = 0.001,
               const int N = 40000,
               const int thin = 1,
               const int burnin = 10000,
-              const int print_freq = 100) {
-  const NumericVector hypers = NumericVector::create(beta, sigma, a_beta, b_beta, s_mu, a_sigma, b_sigma);
+              const int print_freq = 100,
+              const int update_eta = true) {
+  const NumericVector hypers = NumericVector::create(eta, sigma, a_eta, b_eta, s_mu, a_sigma, b_sigma);
   if (is_true(any(hypers <= 0.0))) {
-    stop("mh_model: initial value of beta & other hyperparameters must be positive.");
+    stop("mh_model: initial value of eta & other hyperparameters must be positive.");
   }
   NumericVector gamma_init = rnorm(m, mu, sigma), gamma_curr = clone(gamma_init), gamma_prop = clone(gamma_curr), sds_gamma(m, sd_gamma);
-  double beta_curr = beta, beta_prop,
+  gamma_init[0] = 0.0;
+  gamma_curr[0] = 0.0; // for identifiability?
+  gamma_prop[0] = 0.0;
+  double eta_curr = eta, eta_prop,
     mu_curr = mu, mu_prop,
     sigma_curr = sigma, sigma_prop;
-  auto lpost = [y1, y2, x1, x2, a_beta, b_beta, m_mu, s_mu, a_sigma, b_sigma](const NumericVector gamma, const double beta, const double mu, const double sigma) {
-    double lpost = llik_model(y1, y2, x1, x2, gamma, beta) + lprior_model(gamma, beta, mu, sigma, a_beta, b_beta, m_mu, s_mu, a_sigma, b_sigma);
+  auto lpost = [y1, y2, x1, x2, a_eta, b_eta, m_mu, s_mu, a_sigma, b_sigma](const NumericVector gamma, const double eta, const double mu, const double sigma) {
+    double lpost = llik_model(y1, y2, x1, x2, gamma, eta) + lprior_model(gamma, eta, mu, sigma, a_eta, b_eta, m_mu, s_mu, a_sigma, b_sigma);
     if (lpost != lpost) {
       lpost = -INFINITY;
     }
     return lpost;
   };
-  double lpost_curr = lpost(gamma_curr, beta_curr, mu_curr, sigma_curr), lpost_prop;
+  double lpost_curr = lpost(gamma_curr, eta_curr, mu_curr, sigma_curr), lpost_prop;
   NumericMatrix gamma_mat(N, m);
-  NumericVector beta_vec(N), mu_vec(N), sigma_vec(N);
+  NumericVector eta_vec(N), mu_vec(N), sigma_vec(N), lpost_vec(N);
   // run
   int i, j;
   for (i = 0; i < N * thin + burnin; i++) {
     // update gamma
-    for (j = 0; j < m; j++) {
+    for (j = 1; j < m; j++) { // [0] not updated
       gamma_prop = clone(gamma_curr);
       gamma_prop[j] = rnorm(1, gamma_curr[j], sds_gamma[j])[0];
-      lpost_prop = lpost(gamma_prop, beta_curr, mu_curr, sigma_curr);
+      lpost_prop = lpost(gamma_prop, eta_curr, mu_curr, sigma_curr);
       update(gamma_curr[j], gamma_prop[j], lpost_curr, lpost_prop, sds_gamma[j], i, burnin);
     }
-    // update beta
-    beta_prop = rnorm(1, beta_curr, sd_beta)[0];
-    lpost_prop = lpost(gamma_curr, beta_prop, mu_curr, sigma_curr);
-    update(beta_curr, beta_prop, lpost_curr, lpost_prop, sd_beta, i, burnin);
+    // update eta
+    if (update_eta) {
+      eta_prop = rnorm(1, eta_curr, sd_eta)[0];
+      lpost_prop = lpost(gamma_curr, eta_prop, mu_curr, sigma_curr);
+      update(eta_curr, eta_prop, lpost_curr, lpost_prop, sd_eta, i, burnin);
+    }
     // update mu
     mu_prop = rnorm(1, mu_curr, sd_mu)[0];
-    lpost_prop = lpost(gamma_curr, beta_curr, mu_prop, sigma_curr);
+    lpost_prop = lpost(gamma_curr, eta_curr, mu_prop, sigma_curr);
     update(mu_curr, mu_prop, lpost_curr, lpost_prop, sd_mu, i, burnin);
     // update sigma
     sigma_prop = rnorm(1, sigma_curr, sd_sigma)[0];
-    lpost_prop = lpost(gamma_curr, beta_curr, mu_curr, sigma_prop);
+    lpost_prop = lpost(gamma_curr, eta_curr, mu_curr, sigma_prop);
     update(sigma_curr, sigma_prop, lpost_curr, lpost_prop, sd_sigma, i, burnin);
     // print & save
     if ((i + 1) % print_freq == 0) {
-      Rcout << "Iteration " << i + 1 << endl;
+      Rcout << "Iteration " << i + 1 << ": lpost = " << lpost_curr << endl;
+      Rcout << "eta: " << eta_curr << " (" << sd_eta << ")" << endl;
+      Rcout << "mu: " << mu_curr << " (" << sd_mu << ")" << endl;
+      Rcout << "sigma: " << sigma_curr << " (" << sd_sigma << ")" << endl;
+      Rcout << endl;
     }
     if (i >= burnin && (i - burnin + 1) % thin == 0) {
       j = (i - burnin + 1) / thin - 1;
       gamma_mat(j, _) = gamma_curr;
-      beta_vec[j] = beta_curr;
+      eta_vec[j] = eta_curr;
       mu_vec[j] = mu_curr;
       sigma_vec[j] = sigma_curr;
+      lpost_vec[j] = lpost_curr;
     }
   }
   // output
-  DataFrame par = DataFrame::create(Named("beta") = beta_vec,
-                                    Named("mu") = mu_vec,
-                                    Named("sigma") = sigma_vec),
-    initial = DataFrame::create(Named("beta") = tv(beta),
+  DataFrame lpost_df = DataFrame::create(Named("iteration") = seq_len(N), Named("lpost") = lpost_vec),
+    par = DataFrame::create(Named("eta") = eta_vec,
+                            Named("mu") = mu_vec,
+                            Named("sigma") = sigma_vec),
+    initial = DataFrame::create(Named("eta") = tv(eta),
                                 Named("mu") = tv(mu),
                                 Named("sigma") = tv(sigma)),
-    hyper = DataFrame::create(Named("a_beta") = tv(a_beta),
-                              Named("b_beta") = tv(b_beta),
+    hyper = DataFrame::create(Named("a_eta") = tv(a_eta),
+                              Named("b_eta") = tv(b_eta),
                               Named("m_mu") = tv(m_mu),
                               Named("s_mu") = tv(s_mu),
                               Named("a_sigma") = tv(a_sigma),
                               Named("b_sigma") = tv(b_sigma)),
-    sds = DataFrame::create(Named("sd_beta") = tv(sd_beta),
+    sds = DataFrame::create(Named("sd_eta") = tv(sd_eta),
                             Named("sd_mu") = tv(sd_mu),
                             Named("sd_sigma") = tv(sd_sigma)),
     scalars = DataFrame::create(Named("N") = IntegerVector::create(N),
                                 Named("thin") = IntegerVector::create(thin),
                                 Named("burnin") = IntegerVector::create(burnin),
                                 Named("print_freq") = IntegerVector::create(print_freq));
-  List output = List::create(Named("gamma_par") = gamma_mat, Named("gamma_initial") = gamma_init, Named("gamma_sds") = sds_gamma, Named("par") = par, Named("initial") = initial, Named("hyper") = hyper, Named("sds") = sds, Named("scalars") = scalars);
+  List output = List::create(Named("gamma_par") = gamma_mat, Named("gamma_initial") = gamma_init, Named("gamma_sds") = sds_gamma, Named("lpost") = lpost_df, Named("par") = par, Named("initial") = initial, Named("hyper") = hyper, Named("sds") = sds, Named("scalars") = scalars);
   return output;
 }
--- modelling.R
+++ modelling.R
@@ -1,12 +1,18 @@
 ### 00) prelim
+print(Sys.time())
 library(tidyverse)
 library(magrittr)
+library(coda)
+library(glue)
 library(Rcpp)
 sourceCpp("algo.cpp")
 
 
 
+
+
 ### 01) read-in data
+## matches
 df0.matches <- read_csv(
     "data.csv",
     col_types = c(
@@ -19,18 +25,121 @@ df0.matches <- read_csv(
 )
 n <- df0.matches %>% nrow()
 
+## players
 df0.players <- bind_rows(
     df0.matches %>% select(player = player1),
     df0.matches %>% select(player = player2)
 ) %>%
-    count(player) %>%
-    mutate(id = seq_along(player) %>% subtract(1L))
-m <- df0.players %>% nrow()
+    count(player)
+min.matches <- 3L #### can change
+df1.players <- bind_rows(
+    df0.players %>%
+    filter(n >= min.matches) %>%
+    mutate(id = seq_along(player)),
+    df0.players %>%
+    filter(n < min.matches) %>%
+    mutate(id = 0L)
+)
+m <- df1.players$id %>% unique() %>% length()
 
+## matches again
 df1.matches <- df0.matches %>%
-    left_join(df0.players %>% select(player1 = player, id1 = id), "player1") %>%
-    left_join(df0.players %>% select(player2 = player, id2 = id), "player2")
+    left_join(df1.players %>% select(player1 = player, id1 = id), "player1") %>%
+    left_join(df1.players %>% select(player2 = player, id2 = id), "player2")
+y1 <- df1.matches$games1
+y2 <- df1.matches$games2
+x1 <- df1.matches$id1
+x2 <- df1.matches$id2
 
 
 
-### 02)
+
+
+### 02) Bradley-Terry model
+set.seed(1000L)
+t0 <- system.time({
+    obj0 <- mh_model(
+        y1, y2, x1, x2, m,
+        eta = 1.0,
+        N = 20000L,
+        thin = 10L,
+        burnin = 50000L,
+        update_eta = FALSE
+    )
+})
+
+gamma0 <- obj0$gamma_par
+df0.strength <- tibble(
+    id = seq(m) - 1L,
+    mean = gamma0 %>% colMeans,
+    sd = gamma0 %>% apply(2, sd),
+    lower = gamma0 %>% apply(2, quantile, 0.025),
+    upper = gamma0 %>% apply(2, quantile, 0.975)
+) %>%
+    left_join(df1.players, ., "id") %>%
+    mutate(player = fct_reorder(player, mean))
+
+gg0.strength <- df0.strength %>%
+    top_n(30, mean) %>%
+    ggplot() +
+    geom_segment(aes(lower, player, xend = upper, yend = player)) +
+    geom_point(aes(mean, player)) +
+    theme_bw(15L) +
+    labs(x = "(log-)strength")
+
+
+
+
+
+
+### 03) whole spectrum of beta = 1.0 / eta
+set.seed(2000L)
+t1 <- system.time({
+    obj1 <- mh_model(
+        y1, y2, x1, x2, m,
+        eta = 0.5,
+        N = 20000L,
+        thin = 20L,
+        burnin = 100000L,
+        update_eta = TRUE
+    )
+})
+
+gamma1 <- obj1$gamma_par
+df1.strength <- tibble(
+    id = seq(m) - 1L,
+    mean = gamma1 %>% colMeans,
+    sd = gamma1 %>% apply(2, sd),
+    lower = gamma1 %>% apply(2, quantile, 0.025),
+    upper = gamma1 %>% apply(2, quantile, 0.975)
+) %>%
+    left_join(df1.players, ., "id") %>%
+    mutate(player = fct_reorder(player, mean))
+
+gg1.strength <- df1.strength %>%
+    top_n(30, mean) %>%
+    ggplot() +
+    geom_segment(aes(lower, player, xend = upper, yend = player)) +
+    geom_point(aes(mean, player)) +
+    theme_bw(15L) +
+    labs(x = "(log-)strength")
+
+
+
+
+
+### 04) save & exit
+## Bradley-Terry
+obj0 %>% write_rds("obj0.rds")
+df0.strength %>% write_csv("strengths_B_T.csv")
+print(gg0.strength)
+ggsave("Bradley_Terry_30.jpg")
+
+## whole spectrum
+obj1 %>% write_rds("obj1.rds")
+df1.strength %>% write_csv("strengths_all.csv")
+print(gg1.strength)
+ggsave("Flexible_beta_30.jpg")
+
+##
+print(Sys.time())
